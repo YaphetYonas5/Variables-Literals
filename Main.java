1e973f139e06ae07a868a273a7eb02681415f54b
YaphetYonas5 <yaphet.yonas@hgs.hiddengeniusproject.org>
2022-01-13 02:03:20 +0000

    second commit

---
+++ Main.java
@@ -0,0 +1,10 @@
+class Main {
+  public static void main(String[] args) {
+  String name = ("yaphet");
+  int age = 14;
+  String dreamjob = ("Enginear");
+  System.out.println("my name is " + name);
+  System.out.println("I am " + age);
+  System.out.println("I want to be an " + dreamjob);
+  }
+}
